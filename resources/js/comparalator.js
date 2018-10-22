aa509eea53e2216276883db2b14084deae2aed91
Catherine Winters <catherine@catherinewinters.com>
2018-10-22 15:36:10 -0700

    testing a new method for comparing slected text

--- resources/js/comparalator.js
+++ resources/js/comparalator.js
@@ -38,24 +38,62 @@ $(document).on('mouseup', '#original', function() {
   console.log("moused up");
   // console.log("selectedText: " + getSelectionText());
 
-  var selectedText = window.getSelection();
+  // var selectedText = window.getSelection();
+  //
+  // if (selectedText != "") {
+  //   console.log("selectedText: " + selectedText);
+  //
+  //   // var selectedTextRange = selectedText.getRangeAt(0); //get the text range
+  //   // var selectedTextPos = selectedTextRange.getBoundingClientRect();
+  //   // console.log(selectedTextPos);
+  //   var highlight = window.getSelection(),
+  //     spn = $('<span class="selected-text" data-toggle="popover" title="selected text" data-placement="top auto"></span>')[0],
+  //     range = highlight.getRangeAt(0);
+  //   range.surroundContents(spn);
+  //
+  //   $('[data-toggle="popover"]').popover('show'); // reinitialize popovers
+  //
+  //   // selectedTextString = selectedText.toString();
+  // }
 
-  if (selectedText != "") {
-    console.log("selectedText: " + selectedText);
-
-    // var selectedTextRange = selectedText.getRangeAt(0); //get the text range
-    // var selectedTextPos = selectedTextRange.getBoundingClientRect();
-    // console.log(selectedTextPos);
-    var highlight = window.getSelection(),
-      spn = $('<span class="selected-text" data-toggle="popover" title="selected text" data-placement="top auto"></span>')[0],
-      range = highlight.getRangeAt(0);
-    range.surroundContents(spn);
-
-    $('[data-toggle="popover"]').popover('show'); // reinitialize popovers
-
-    // selectedTextString = selectedText.toString();
-  }
+  highlight("red");
 });
+
+
+function makeEditableAndHighlight(colour) {
+  sel = window.getSelection();
+  if (sel.rangeCount && sel.getRangeAt) {
+    range = sel.getRangeAt(0);
+  }
+  document.designMode = "on";
+  if (range) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+  // Use HiliteColor since some browsers apply BackColor to the whole block
+  if (!document.execCommand("HiliteColor", false, colour)) {
+    document.execCommand("BackColor", false, colour);
+  }
+  document.designMode = "off";
+}
+
+function highlight(colour) {
+  var range, sel;
+  if (window.getSelection) {
+    // IE9 and non-IE
+    try {
+      if (!document.execCommand("BackColor", false, colour)) {
+        makeEditableAndHighlight(colour);
+      }
+    } catch (ex) {
+      makeEditableAndHighlight(colour)
+    }
+  } else if (document.selection && document.selection.createRange) {
+    // IE <= 8 case
+    range = document.selection.createRange();
+    range.execCommand("BackColor", false, colour);
+  }
+}
 
 
 
