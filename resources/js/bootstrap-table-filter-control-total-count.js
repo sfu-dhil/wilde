e077bca1a77203db1d53701a62471c01177c83d2
Catherine Winters <catherine@catherinewinters.com>
2018-11-07 11:49:26 -0800

    Added total count to filter control

--- resources/js/bootstrap-table-filter-control-total-count.js
+++ resources/js/bootstrap-table-filter-control-total-count.js
@@ -213,7 +213,7 @@
 
                     uniqueValues[formattedValue] = fieldValue;
 
-                    console.log("total number of " + fieldValue + ": " + data.filter(value => fieldValue === fieldValue).length);
+                    console.log("total number of " + fieldValue + ": " + data.filter(value => that.header.formatters[j] === fieldValue).length);
                 }
 
                 // console.log("all: " + JSON.stringify(data));
