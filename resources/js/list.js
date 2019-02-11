7bd651a12decbdd01638311ec18e9bddb94c4cf7
Michael Joyce <ubermichael@gmail.com>
2019-02-11 12:03:20 -0800

    restore deleted code. my bad.

--- resources/js/list.js
+++ resources/js/list.js
@@ -7,3 +7,29 @@ $('.nav-tabs a').click(function (e) {
   window.location.hash = this.hash;
   $('html,body').scrollTop(scrollmem);
 });
+
+// debugging bootstraptable.
+// console.log("Started.");
+// $('#tbl-browser').on('onAll.bs.table', function (e, arg1, arg2) {
+//     console.log("event");
+// });
+
+// $('#tbl-browser').bootstrapTable({
+//   filterStrictSearch: true,
+//   onAll: function (whatChanged, changedTo) {
+//     if (whatChanged == "column-search.bs.table") {
+//       console.log("The table changed!");
+//
+//       var field_that_changed = changedTo[0];//changedTo.split(',').slice(0).join(',')
+//       console.log(field_that_changed);
+//
+//       // if
+//       // "bootstrap-table-filter-control-city"
+//       if (field_that_changed == "region") {
+//         $('.bootstrap-table-filter-control-city').val("");
+//       }
+//     }
+//     // console.log(whatChanged + " !!! " + changedTo);
+//   }
+// });
+//   onAll: function (whatChanged, changedTo) {
