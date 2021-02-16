8a56199aa1fa5de20fbd0fec20189d46f4d1dd98
Joey Takeda <joey.takeda@gmail.com>
2021-02-16 15:31:25 -0800

    More work on the documentation and adding demonstrative image

--- resources/js/page/documentation.js
+++ resources/js/page/documentation.js
@@ -4,10 +4,12 @@
 // Set up the TOC to do smooth scrolling, when possible:
 
 const toc = document.getElementById('toc');
+const links = toc.querySelectorAll('a[href^="#"]'); 
 
-toc.querySelectorAll('a[href^="#"]').forEach(link => {
+links.forEach(link => {
    link.addEventListener('click', e => {
       e.preventDefault();
       document.querySelector(link.getAttribute('href')).scrollIntoView({behavior: "smooth"});
    });
 });
+
